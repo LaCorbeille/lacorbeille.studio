33ccbdd0a40a2e7a1feb3318eb2b0a9f26ad904f
Noa Second <contact@noasecond.com>
2024-07-24 14:43:23 +0200

    :sparkles: Add mobile header

---
+++ js/header.js
@@ -0,0 +1,8 @@
+document.getElementById('burgerMenu').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+    } else {
+        menu.classList.add('active');
+    }
+});
