5aaec840b07e029d60de2c4053dde083adeb28a3
Noa Second <contact@noasecond.com>
2024-07-25 10:50:21 +0200

    :sparkles: Add terms PopUp

--- signup.php
+++ signup.php
@@ -4,6 +4,7 @@
 <head>
     <?php include 'components/head.php'; ?>
     <link rel="stylesheet" href="css/signin.css">
+    <script src="js/popUp.js" defer></script>
 </head>
 
 <body>
@@ -14,10 +15,15 @@
             <input type="email" name="email" placeholder="Email" required>
             <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
-            <label id="stayConnected"><input type="checkbox" name="remember" value="true"> Accepter les conditions générales</label>
+            <label id="stayConnected"><input type="checkbox" name="remember" value="true"> Accepter les&nbsp;<a id="openPopUp">conditions générales</a></label>
             <button type="submit">Créer le compte</button>
             <a href="signin.php" id="bottomText">Se connecter</a>
         </form>
+        <div id="popUp">
+            <h2>Conditions générales</h2>
+            <p>En cochant cette case, vous acceptez les conditions générales d'utilisation de LaCorbeille STUDIO.</p>
+            <button id="closePopUp">Fermer</button>
+        </div>
     </main>
     <?php include 'components/footer.php'; ?>
 </body>
