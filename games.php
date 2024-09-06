bba9533cc57cb01dc7329e200a7682efca27b48c
𝕭𝖗𝖚𝖒𝖊 <contact@noasecond.com>
2024-09-06 21:35:35 +0200

    :lipstick: Update game UI

--- games.php
+++ games.php
@@ -15,24 +15,32 @@ if (session_status() === PHP_SESSION_NONE) {
 <body>
     <?php include 'components/header.php'; ?>
     <main>
-        <?php
-        $title = 'A Little Adventue';
-        $platforms = ['Windows', 'Switch'];
-        $img = 'https://placehold.co/400x600?text=A+Little+Adventure';
-        include 'components/games/game.php';
-        ?>
-        <?php
-        $title = 'Platformer 3D';
-        $platforms = ['Windows'];
-        $img = 'https://placehold.co/400x600?text=Platformer+3D';
-        include 'components/games/game.php';
-        ?>
-        <?php
-        $title = 'BOT A.N.I.C';
-        $platforms = ['Windows', 'Linux', 'XBox', 'PlayStation'];
-        $img = 'https://placehold.co/400x600?text=BOT+A.N.I.C';
-        include 'components/games/game.php';
-        ?>
+        <div id="gamesWrapper">
+            <?php
+            $title = 'Platformer 3D';
+            $platforms = ['Windows'];
+            $img = 'assets/img/games/Platformer3D.png';
+            include 'components/games/game.php';
+            ?>
+            <?php
+            $title = 'Rice Battle';
+            $platforms = ['Windows'];
+            $img = 'assets/img/games/RiceBattle.png';
+            include 'components/games/game.php';
+            ?>
+            <?php
+            $title = 'A Little Adventue';
+            $platforms = ['Windows', 'Switch'];
+            $img = 'https://placehold.co/400x600?text=A+Little+Adventure';
+            include 'components/games/game.php';
+            ?>
+            <?php
+            $title = 'BOT A.N.I.C';
+            $platforms = ['Windows', 'Linux', 'XBox', 'PlayStation'];
+            $img = 'https://placehold.co/400x600?text=BOT+A.N.I.C';
+            include 'components/games/game.php';
+            ?>
+        </div>
     </main>
     <?php include 'components/footer.php'; ?>
 </body>
