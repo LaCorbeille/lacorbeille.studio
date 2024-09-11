bdcc5909120ba3cc0b18d7ff016661d1b2218f47
𝕭𝖗𝖚𝖒𝖊 <contact@noasecond.com>
2024-09-12 00:04:45 +0200

    :dizzy: Add animations and transitions

--- index.php
+++ index.php
@@ -18,6 +18,12 @@ if (session_status() === PHP_SESSION_NONE) {
     include 'components/header.php';
     ?>
     <main>
+        <div class="loadingContainer">
+            <svg class="loadingSVGContainer" viewBox="0 0 40 40" height="40" width="40">
+                <circle class="track" cx="20" cy="20" r="17.5" pathlength="100" stroke-width="5px" fill="none" />
+                <circle class="car" cx="20" cy="20" r="17.5" pathlength="100" stroke-width="5px" fill="none" />
+            </svg>
+        </div>
         <img id="maintenanceImg" src="assets/img/branding/logoFull.svg" alt="LaCorbeille STUDIO">
     </main>
     <?php include 'components/footer.php'; ?>
