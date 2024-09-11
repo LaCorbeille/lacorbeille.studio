d282efc997c421c52123d786f3a036ec49d32ff2
𝕭𝖗𝖚𝖒𝖊 <contact@noasecond.com>
2024-09-11 21:34:40 +0200

    :sparkles: Add news content

--- components/footer.php
+++ components/footer.php
@@ -1,13 +1,11 @@
 <footer>
     <div id="footerTop">
         <div id="socialWrap">
-            <a href="#" target="_blank"><img src="assets/img/social/LinkedIn.svg" alt="LinkedIn"></a>
-            <a href="https://www.instagram.com/lacorbeille.studio" target="_blank"><img
-                    src="assets/img/social/Instagram.svg" alt="Instagram"></a>
-            <a href="#" target="_blank"><img src="assets/img/social/X.svg" alt="X"></a>
-            <a href="#" target="_blank"><img src="assets/img/social/Facebook.svg" alt="Facebook"></a>
-            <a href="https://discord.gg/hmPzS4k" target="_blank"><img src="assets/img/social/Discord.svg"
-                    alt="Discord"></a>
+            <a href="https://www.linkedin.com/company/lacorbeille-studio" target="_blank"><img src="assets/img/social/LinkedIn.svg" alt="LinkedIn"></a>
+            <a href="https://www.instagram.com/lacorbeille.studio" target="_blank"><img src="assets/img/social/Instagram.svg" alt="Instagram"></a>
+            <a href="https://x.com/LaCorbeilleSTD" target="_blank"><img src="assets/img/social/X.svg" alt="X"></a>
+            <a href="https://www.facebook.com/people/LaCorbeille-Studio/61565357266191/" target="_blank"><img src="assets/img/social/Facebook.svg" alt="Facebook"></a>
+            <a href="https://discord.gg/hmPzS4k" target="_blank"><img src="assets/img/social/Discord.svg" alt="Discord"></a>
         </div>
         <img src="assets/img/branding/logoFullWhite.svg" alt="LaCorbeille STUDIO">
         <a id="copyright">© LaCorbeille STUDIO 2024</a>
