24acdc53c0cf5ed2e128cb5979d80c6447278d31
𝕭𝖗𝖚𝖒𝖊 <contact@noasecond.com>
2024-09-21 23:03:55 +0200

    :fire: Remove auto database init

--- scripts/connectDB.php
+++ scripts/connectDB.php
@@ -21,17 +21,6 @@ function connectToDB()
     try {
         $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // If the connection is successful, check if the table accounts exists
-        $stmt = $conn->query("SELECT 1 FROM accounts LIMIT 1");
-        if ($stmt === false) {
-            // If the table does not exist, create it with initDB.sql
-            $sql = file_get_contents(__DIR__ . '/../scripts/initDB.sql');
-            $conn->exec($sql);
-        } else {
-            $stmt->closeCursor();
-        }
-
         return $conn;
     } catch (PDOException $e) {
         // echo "Connection failed: " . $e->getMessage();
